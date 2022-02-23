b5e2174d6a7e40ae4d671284c579ec4b4ff9f9a0
d3n0 <deivis.norvaisas@gmail.com>
2022-02-23 21:02:11 +0200

    done with a task 8

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,18 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+const Calculator = () => {
+    this.sum = (first, second) => {
+        return first+second;
+    };
+    this.subtraction = (first, second) => {
+        return first-second;
+    };
+    this.multiplication = (first, second) => {
+        return first*second;
+    };
+    this.division = (first, second) => {
+        return first / second;
+    };
+};
