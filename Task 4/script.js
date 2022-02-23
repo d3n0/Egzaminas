d7fbc5790558bdd9de95977dc7cae5476b166fa2
d3n0 <deivis.norvaisas@gmail.com>
2022-02-23 20:01:11 +0200

    Task 4 done

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,24 @@ bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+fetch(ENDPOINT)
+.then(response => response.json())
+.then(cars => {
+    const output = document.getElementById('output');
+    cars.forEach(car => {
+        let card = document.createElement('div');
+        output.append(card);
+        let title = document.createElement('h4');
+        title.textContent = car.brand;
+        card.append(title);
+        let modelsList = document.createElement('ul');
+        card.append(modelsList);
+
+        car.models.forEach(model => {
+            let listItem = document.createElement('li');
+            listItem.textContent = model;
+            modelsList.append(listItem);
+        });
+    });
+});
